cf038d584615fa8f718675e462c81b73d18fc525
dagm19 <drwn03gm@gmail.com>
2024-05-23 14:05:30 -0600

    [ADD] Modulo que adiciona la funcionalidad de obtener las deducciones de forma quincenal

--- hr_payroll_cm/models/payroll.py
+++ hr_payroll_cm/models/payroll.py
@@ -1,53 +1,49 @@
-from math import ceil
+from odoo import api, fields, models, _
 
 from dateutil.relativedelta import relativedelta
-
-from odoo import api, fields, models
 
 
 class HrSalaryAttachment(models.Model):
     _inherit = 'hr.salary.attachment'
 
     first_date_payment = fields.Date(
-        'Fecha de inicio de pago',
-        compute='_compute_first_date_payment',
+        'Fecha de inicio del pago', default=False, tracking=True,
         help='Fecha de comienzo de pagos', )
 
-    @api.depends('state', 'total_amount', 'monthly_amount', 'date_start', 'first_date_payment')
+    @api.depends('state', 'total_amount', 'monthly_amount', 'date_start')
     def _compute_estimated_end(self):
         for record in self:
             if record.state not in ['close', 'cancel'] and record.total_amount and record.monthly_amount:
-                payments = record._compute_number_of_payments(record.total_amount, record.monthly_amount)
-                record._compute_date_estimated_end(payments)
+                rate = self.compute_rate()
+                record.compute_first_date_payment()
+                record.compute_date_estimated_end(rate)
             else:
-                record.date_estimated_end = False
+                record.date_estimated_end = None
+                record.first_date_payment = None
 
-    @staticmethod
-    def _compute_number_of_payments(total_amount, monthly_amount):
-        if monthly_amount == 0:
-            return 0
-        number_of_payments = ceil(total_amount / monthly_amount)
-        return number_of_payments
+    def compute_rate(self):
+        rate = self.total_amount / self.monthly_amount
 
-    @api.depends('date_start')
-    def _compute_first_date_payment(self):
-        for record in self:
-            if record.date_start.day > 15:
-                record.first_date_payment = record.date_start + relativedelta(months=1, day=1)
-            elif record.date_start.day == 1:
-                record.first_date_payment = record.date_start
-            else:
-                record.first_date_payment = record.date_start + relativedelta(day=15)
+        return rate
 
-    def _compute_date_estimated_end(self, number_of_payments):
-        for record in self:
-            num_moths = int((number_of_payments - 1) // 2)
-            num_days = int(((number_of_payments - 1) % 2) * 15)
+    def compute_first_date_payment(self):
+        self.first_date_payment = self.date_start.replace(
+            month=self.date_start.month
+            if self.date_start.day <= 15
+            else self.date_start.month + 1,
+            day=15 if 1 < self.date_start.day <= 15 else 1
+        )
 
-            if record.first_date_payment.day == 15 and num_days == 15:
-                num_moths += 1
-                num_days = 1
+    def compute_date_estimated_end(self, rate):
+        num_moths = (rate - 1) // 2
+        num_days = 15 if ((rate - 1) % 2) > 0 else 0
+        num_moths = int(num_moths)
+        num_days = int(num_days)
 
-            record.date_estimated_end = record.first_date_payment + relativedelta(
-                months=num_moths, day=num_days or record.first_date_payment.day
-                )
+        if self.first_date_payment.day == 15 and num_days == 15:
+            num_moths += 1
+            num_days = 1
+
+        self.date_estimated_end = self.first_date_payment + relativedelta(
+            months=num_moths, day=num_days or self.first_date_payment.day
+        )
