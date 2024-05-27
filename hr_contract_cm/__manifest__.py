6bf9c6f8bbcaf502b59487ad3e62557b330d31d5
dagm19 <drwn03gm@gmail.com>
2024-05-27 15:57:58 -0600

    [ADD] Campos adicionales para el Bono de Transporte en el Módulo de Empleados/Contrato de Recursos Humanos

---
+++ hr_contract_cm/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Transportation bonus module for CM Airlines',
+    'version': '1.0',
+    'category': 'Human Resources/Contract',
+    'sequence': 244,
+    'summary': 'Transportation bonus module for CM Airlines',
+    'description': """
+This module has added a series of fields for the management of transportation bonuses 
+for CM Airlines employees.
+        """,
+    'depends': ['hr_contract', 'hr'],
+    'data': [
+        'views/hr_contract_bonus_transport_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+
+}
