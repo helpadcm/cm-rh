9ea67a50c3b810029b0c81784f5fbe5360b55f80
dagm19 <drwn03gm@gmail.com>
2024-05-30 13:47:56 -0600

    [REF] Se cambio el comportamiento de del metodo calculate_bonus_for_record ahora no calcula el importe total , si no la cantidad de bonos que se van a otorgar

--- hr_payroll_cm/models/payslip.py
+++ hr_payroll_cm/models/payslip.py
@@ -1,3 +1,5 @@
+import logging
+
 from odoo import models
 import dateutil
 
@@ -66,93 +68,41 @@ class HrPayslipBonus(models.Model):
 
             self.env['hr.payslip.worked_days'].create(worked_day_lines)
 
-    def calculate_bonus_for_record(self,
-                                   work_entry, bonus_rate, early_checkin_bonus_time, late_checkout_bonus_time
-                                   ):
-
-        """
-          Calculate the bonus for a single work entry.
-
-          Parameters:
-          work_entry (dict): The work entry.
-          bonus_rate (float): The bonus rate.
-          early_checkin_bonus_time (time): The time before which an early check-in bonus is assigned.
-          late_checkout_bonus_time (time): The time after which a late check-out bonus is assigned.
-
-          Returns:
-          float: The bonus for the work entry.
-        """
-
+    def _count_bonus_for_record(self, work_entry,
+                                early_checkin_bonus_time,
+                                late_checkout_bonus_time):
         tegucigalpa_tz = dateutil.tz.gettz('America/Tegucigalpa')
         date_start = work_entry["date_start"].astimezone(tegucigalpa_tz)
         date_stop = work_entry["date_stop"].astimezone(tegucigalpa_tz)
-        bonus = 0
-        checkin_time = (
-                date_start.time().hour
-                + date_start.time().minute / 60
-        )
-        checkout_time = (
-                date_stop.time().hour
-                + date_stop.time().minute / 60
-        )
+        bonus_count = 0
+        checkin_time = date_start.time().hour + date_start.time().minute / 60
+        checkout_time = date_stop.time().hour + date_stop.time().minute / 60
 
         if checkin_time <= early_checkin_bonus_time:
-            bonus += bonus_rate
+            bonus_count += 1
 
         if checkout_time >= late_checkout_bonus_time:
-            bonus += bonus_rate
+            bonus_count += 1
 
-        return bonus
+        return bonus_count
 
-    def calculate_transport_bonus(self,
-                                  work_entries,
-                                  max_bonus,
-                                  bonus_rate,
-                                  early_checkin_bonus_time,
-                                  late_checkout_bonus_time,
-                                  ):
-        """
-          Calculate the transport bonus based on a list of work entries,
-          bonus limit, bonus_rate time_upper_limit and time_lower_limit.
-
-          Parameters:
-          work_entries (list): A list of work entries.
-          max_bonus (float): The bonus limit.
-          bonus_rate (float): The bonus rate.
-          early_checkin_bonus_time (time): The upper bond of time. Before this time, the bonus is assigned.
-          late_checkout_bonus_time (time): The lower limit of hours. After this time, the bonus is assigned.
-
-          Returns:
-          float: The transport bonus.
-        """
-        bonus = 0
+    def _calculate_transport_bonus(self,
+                                   work_entries,
+                                   max_bonus,
+                                   early_checkin_bonus_time,
+                                   late_checkout_bonus_time):
+        bonus_count = 0
         for work_entry in work_entries:
-            bonus += self.calculate_bonus_for_record(
-                work_entry,
-                bonus_rate,
-                early_checkin_bonus_time,
-                late_checkout_bonus_time,
-            )
-            if bonus >= max_bonus:
+            bonus_count += self._count_bonus_for_record(work_entry,
+                                                        early_checkin_bonus_time,
+                                                        late_checkout_bonus_time)
+            if bonus_count >= max_bonus:
                 break
-        return min(bonus, max_bonus)
+        return min(bonus_count, max_bonus)
 
-    def get_employee_work_entries(self,
-                                  employee_id,
-                                  work_entries_start_date,
-                                  work_entries_end_date
-                                  ):
-        """
-          Get the work entries for a specific employee within a date range.
-
-          Parameters:
-          employee_id (int): The ID of the employee.
-          work_entries_start_date (date): The start date of the work entries period.
-          work_entries_end_date (date): The end date of the work entries period.
-
-          Returns:
-          list: A list of work entries.
-        """
+    def _get_employee_work_entries(self, employee_id,
+                                   work_entries_start_date,
+                                   work_entries_end_date):
         work_entries = self.env['hr.work.entry'].search(
             [
                 ("employee_id", "=", employee_id.id),
@@ -164,32 +114,29 @@ class HrPayslipBonus(models.Model):
         return work_entries
 
     def action_calculate_transport_bonus_for_payslip(self):
-        """
-        Calculate the transport bonus for each employee based on their work entries.
-        """
-
         for payslip in self:
             employee_id = payslip.employee_id
-            work_entries = self.get_employee_work_entries(employee_id, payslip.date_from, payslip.date_to)
-            max_bonus = payslip.contract_id.transportation_bonus
-            bonus_rate = 100
+            work_entries = self._get_employee_work_entries(employee_id, payslip.date_from, payslip.date_to)
+            max_bonus = payslip.contract_id.max_extra_hours
+            bonus_rate = payslip.contract_id.value_bonus
             early_checkin_bonus_time = payslip.contract_id.early_checkin_bonus_time
             late_checkout_bonus_time = payslip.contract_id.late_checkout_bonus_time
-            if not payslip.contract_id.transportation_bonus:
+            if not payslip.contract_id.value_bonus:
                 return 0
-            transport_bonus = self.calculate_transport_bonus(
+            transport_bonus_count = self._calculate_transport_bonus(
                 work_entries,
                 max_bonus,
-                bonus_rate,
                 early_checkin_bonus_time,
                 late_checkout_bonus_time,
             )
-            input_line_values = {
-                "name": "Transport Bonus",
-                "code": "TRANSBONUS",
-                "amount": transport_bonus,
-                "contract_id": payslip.contract_id.id,
-                "payslip_id": payslip.id,
-                "input_type_id": payslip.env['hr.payslip.input.type'].search([("code", "=", "TRANSBONUS")])[0].id,
-            }
-            self.env["hr.payslip.input"].create(input_line_values)
+            logging.info(f"Transport Bonus Count: {transport_bonus_count}")
+            if transport_bonus_count > 0:
+                input_line_values = {
+                    "name": f"Otorgados {transport_bonus_count} Bonos de Transporte",
+                    "code": "TRANSBONUS",
+                    "amount": transport_bonus_count * bonus_rate,
+                    "contract_id": payslip.contract_id.id,
+                    "payslip_id": payslip.id,
+                    "input_type_id": payslip.env['hr.payslip.input.type'].search([("code", "=", "TRANSBONUS")])[0].id,
+                }
+                self.env["hr.payslip.input"].create(input_line_values)
