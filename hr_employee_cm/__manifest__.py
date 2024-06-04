391fefaf495309ae4cba043f41a7588a3a850c78
dagm19 <drwn03gm@gmail.com>
2024-06-04 15:48:26 -0600

    [ADD] Implementación de metodo de formato para 'identification_id' basada en la estructura del DNI hondureño

--- hr_employee_cm/__manifest__.py
+++ hr_employee_cm/__manifest__.py
@@ -7,7 +7,8 @@
     'summary': 'Employee module for CM Airlines',
     'description': """
         This module provides the necessary functionality for CM Airlines.
-        - Gets the employees who have birthdays during the month
+        - Gets the employees who have birthdays during the month.
+        - Formats the employee's identification number to the Honduran standard of exactly 13 digits.
         """,
     'depends': ['hr', ],
     'data': [
