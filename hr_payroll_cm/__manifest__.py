e3de8a5cf0e045e2c675dc323937f021107493dc
helpad <helmutpadilla@gmail.com>
2024-06-04 11:46:03 -0600

    [ADD] Subir nomina a partir de excel de finanzas, funcional para junio de 2024, es necesario hacer mas cambios para estar funcional para todo el tiempo

--- hr_payroll_cm/__manifest__.py
+++ hr_payroll_cm/__manifest__.py
@@ -3,7 +3,7 @@
     'name': 'Payroll module for CM Airlines',
     'version': '1.0',
     'category': 'Human Resources/Payroll',
-    'sequence': 243 ,
+    'sequence': 243,
     'summary': 'Planning module for CM Airlines',
     'description': """
         This module provides the necessary functionality for CM Airlines.
@@ -11,13 +11,16 @@
         """,
     'depends': ['hr_payroll', 'hr_contract_cm'],
     'data': [
+        'security/ir.model.access.csv',
         'views/hr_payroll_views.xml',
         'views/hr_employee_views.xml',
+        'views/hr_payslip_run_views.xml',
         'views/hr_payslip_views.xml',
-    ],
+        'wizard/load_nomina_from_excel_wizard_views.xml',
+        ],
     'installable': True,
     'application': False,
     'auto_install': False,
     'license': 'LGPL-3',
 
-}
+    }
---
+++ hr_payroll_cm/models/payslip_run.py
@@ -0,0 +1,18 @@
+from odoo import fields, models
+
+class HrPayslipRun(models.Model):
+    _inherit = 'hr.payslip.run'
+
+    def action_load_nomina_from_excel_wizard(self):
+        """
+        Open the wizard to load the payroll from an excel file.
+        """
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'load.nomina.from.excel.wizard',
+            'view_mode': 'form',
+            'target': 'new',
+            'context': {
+                'default_payslip_run_id': self.id
+            }
+        }
