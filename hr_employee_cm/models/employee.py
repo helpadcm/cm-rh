6579ed2e9e74ad5e25bb6d8ad752e4e1d4b71b28
dagm19 <drwn03gm@gmail.com>
2024-06-04 15:48:26 -0600

    [ADD] Implementación de metodo de formato para 'identification_id' basada en la estructura del DNI hondureño

--- hr_employee_cm/models/employee.py
+++ hr_employee_cm/models/employee.py
@@ -1,11 +1,13 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
 from dateutil.relativedelta import relativedelta
+import re
 
 
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    next_birthday = fields.Date(string="Proximo Cumpleaños" , compute='_compute_next_birthday')
+    next_birthday = fields.Date(string="Proximo Cumpleaños", compute='_compute_next_birthday')
+
     @api.depends('birthday')
     def _compute_next_birthday(self):
         today = fields.Date.context_today(self)
@@ -17,3 +19,17 @@ class HrEmployee(models.Model):
                 record.next_birthday = next_birthday
             else:
                 record.next_birthday = False
+
+    @api.onchange('identification_id')
+    def _format_identification_id(self):
+        for record in self:
+            if record.identification_id:
+                formatted_id = record.identification_id.strip().replace('-', '')
+                if re.fullmatch("^[0-9]{13}$", formatted_id):
+                    record.identification_id = formatted_id
+                else:
+                    record.identification_id = False
+                    raise exceptions.UserError(
+                        """El número de identificación debe ser de 13 dígitos.""")
+            else:
+                record.identification_id = False
