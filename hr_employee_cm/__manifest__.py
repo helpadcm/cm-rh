179946b149547c1a1848c328d1a4fba1a93eb5f8
dagm19 <drwn03gm@gmail.com>
2024-06-21 13:33:05 -0600

    [REF] Traslado de archivos de employee_public.py y su vista XML desde el módulo hr_payroll_cm a un módulo más apropiado hr_employee_cm

--- hr_employee_cm/__manifest__.py
+++ hr_employee_cm/__manifest__.py
@@ -13,6 +13,8 @@
     'depends': ['hr', ],
     'data': [
         'views/hr_employee_views.xml',
+        'views/hr_employee_public_views.xml'
+        #'data/crm_create_user.xml',
     ],
     'installable': True,
     'application': False,
