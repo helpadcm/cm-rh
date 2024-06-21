029e3e3e9b6eed30ac2d46e80fac4c1f0721c135
dagm19 <drwn03gm@gmail.com>
2024-06-21 13:36:47 -0600

    [ADD] Creacion de una accion planeada que creara usuarios a los empleados a partir del correo de la empresa

--- hr_employee_cm/__manifest__.py
+++ hr_employee_cm/__manifest__.py
@@ -14,7 +14,7 @@
     'data': [
         'views/hr_employee_views.xml',
         'views/hr_employee_public_views.xml'
-        #'data/crm_create_user.xml',
+        'data/crm_create_user.xml',
     ],
     'installable': True,
     'application': False,
--- hr_employee_cm/models/employee.py
+++ hr_employee_cm/models/employee.py
@@ -1,12 +1,24 @@
 from odoo import models, fields, api, exceptions
 from dateutil.relativedelta import relativedelta
+import logging
 import re
+
 
 
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
     next_birthday = fields.Date(string="Proximo Cumpleaños", compute='_compute_next_birthday')
+    employee_no = fields.Char(
+        string="Código de Empleado",
+        help="Este código es único para cada empleado y se utiliza para identificarlo en el sistema.",
+        compute="get_employee_no"
+        )
+
+    def get_employee_no(self):
+        for record in self:
+            record.employee_no = record['registration_number'] or record['barcode'] or record['pin'] or ''
+
 
     @api.depends('birthday')
     def _compute_next_birthday(self):
@@ -34,3 +46,35 @@ class HrEmployee(models.Model):
                         f"Tome como referencia el siguiente ejemplo: 0801199912345\n")
             else:
                 record.identification_id = False
+
+    @api.model
+    def crm_create_user(self):
+        _logger = logging.getLogger(__name__)
+        employees = self.search([])
+
+        Users = self.env['res.users']
+
+        for employee in employees:
+
+            work_email = employee.work_email
+
+            if work_email:
+                username, domain = work_email.split('@')
+
+                if not employee.user_id:
+                    existing_user = Users.search([('login', '=', work_email)], limit=1)
+                    if not existing_user:
+                        new_user = Users.create({
+                            'login': work_email,
+                            'name': username,
+                            'email': work_email,
+                            'groups_id': [(6, 0, [self.env.ref('base.group_portal').id])],
+                        })
+                        employee.user_id = new_user.id
+                        _logger.info(f'Se creó el usuario: {username}')
+                    else:
+                        _logger.info(f'El usuario con el correo electrónico {work_email} ya existe.')
+                else:
+                    _logger.info(f'El empleado {username} ya tiene un usuario asociado.')
+            else:
+                _logger.info(f'El empleado {employee.name} no tiene un correo electrónico de trabajo.')
