8de53a98d1b870610c5babe2535c9b23d3f5b5cc
helpad <helmutpadilla@gmail.com>
2024-07-05 12:07:03 -0600

    [ADD] Se adicionaron las branch o estaciones, esto con el proposito de manejar a que rama de la empresa pertenece un recurso, y manejar mejor las responsabilidades de estos

---
+++ hr_employee_cm/models/branch.py
@@ -0,0 +1,20 @@
+from odoo import fields, models
+
+
+class Branch(models.Model):
+    _name = 'hr.branch'
+    _description = 'Branch'
+
+    name = fields.Char('Name', required=True)
+    code = fields.Char('Code', required=True)
+    company_id = fields.Many2one('res.company', string='Company', required=True)
+    parent_id = fields.Many2one('hr.branch', string='Parent Branch')
+    child_ids = fields.One2many('hr.branch', 'parent_id', string='Child Branches')
+    manager_id = fields.Many2one('hr.employee', string='Manager')
+    employee_ids = fields.One2many('hr.employee', 'branch_id', string='Employees')
+    active = fields.Boolean('Active', default=True)
+    color = fields.Integer('Color Index')
+
+    _sql_constraints = [
+        ('code_uniq', 'unique (code)', "The code of the branch must be unique!"),
+    ]
