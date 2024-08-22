090a58f130ac058cbb8ecfa88648819ce89195f2
helpad <helmutpadilla@gmail.com>
2024-08-22 15:39:29 -0600

    [FIX] Por alguna razon se habia movido la funcion afuera de la clase,

--- stock_cm/models/stock_quant.py
+++ stock_cm/models/stock_quant.py
@@ -1,9 +1,11 @@
-from odoo import api, fields, models
+from odoo import api, models
+
+
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
-@api.model
-def action_view_quants(self):
-    self = self.with_context(search_default_internal_loc=1, search_default_locationgroup=1)
-    self = self._set_view_context()
-    return self._get_quants_action(extend=True)
 
+    @api.model
+    def action_view_quants(self):
+        self = self.with_context(search_default_internal_loc=1, search_default_locationgroup=1)
+        self = self._set_view_context()
+        return self._get_quants_action(extend=True)
