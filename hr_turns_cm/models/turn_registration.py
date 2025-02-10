4725431348a0795d8e012f5033fb963e5d7447f1
Oniel <helmutpadilla@cmairlines.com>
2025-02-09 23:26:12 -0600

    [IMP]envio de correo aunque se crean manualmente

--- hr_turns_cm/models/turn_registration.py
+++ hr_turns_cm/models/turn_registration.py
@@ -116,72 +116,81 @@ class turnRegistration(models.Model):
         first_date = actual_date
         ult_date = actual_date + timedelta(days=6)
         actual_name = 'Semana %s al %s'%(first_date.strftime('%d/%m/%Y'), ult_date.strftime('%d/%m/%Y'))
-        
         team_ids = self.env['hr.work.teams'].search([])
         for team in team_ids:
-            validation = self.validate_dates(team, first_date, ult_date)
-            if validation:
-                created_turn = []
-                for member in team.member_employees_ids:
-                    if member.template_id:
-                        count_days = 0
-                        for day in range(7):
-                            turn_date = actual_date + timedelta(days=count_days)
-                            line_temp_id = self.get_template_line(member.template_id, day)
-                            fortnight_id = self.get_fortnight(turn_date)
-                            oh = 0
-                            oh_value = 0
-                            aditional_time = 0
-                            contract_id = member.employee_id.sudo().contract_id
-                            try:
-                                amount1 = float(line_temp_id.schedule1_in_id.name) - float(line_temp_id.schedule1_out_id.name)
-                                amount2 = float(line_temp_id.schedule2_in_id.name) - float(line_temp_id.schedule2_out_id.name)
-                                oh = abs((amount1 + amount2) / 100)
-                                day = turn_date.weekday()
-                                if day == 5:
-                                    aditional_time = oh - 4
-                                    oh_value = contract_id.sudo().weekend_hours
-                                elif day == 6:
-                                    aditional_time = 0
-                                    oh_value = contract_id.sudo().weekend_hours
-                                else:
-                                    aditional_time = oh - 8
-                                    oh_value = 8
-                            except:
+            name_exist = self.validate_name(team, actual_name)
+            if not name_exist:
+                validation = self.validate_dates(team, first_date, ult_date)
+                if validation:
+                    created_turn = []
+                    for member in team.member_employees_ids:
+                        if member.template_id:
+                            count_days = 0
+                            for day in range(7):
+                                turn_date = actual_date + timedelta(days=count_days)
+                                line_temp_id = self.get_template_line(member.template_id, day)
+                                fortnight_id = self.get_fortnight(turn_date)
                                 oh = 0
+                                oh_value = 0
+                                aditional_time = 0
+                                contract_id = member.employee_id.sudo().contract_id
+                                try:
+                                    amount1 = float(line_temp_id.schedule1_in_id.name) - float(line_temp_id.schedule1_out_id.name)
+                                    amount2 = float(line_temp_id.schedule2_in_id.name) - float(line_temp_id.schedule2_out_id.name)
+                                    oh = abs((amount1 + amount2) / 100)
+                                    day = turn_date.weekday()
+                                    if day == 5:
+                                        aditional_time = oh - 4
+                                        oh_value = contract_id.sudo().weekend_hours
+                                    elif day == 6:
+                                        aditional_time = 0
+                                        oh_value = contract_id.sudo().weekend_hours
+                                    else:
+                                        aditional_time = oh - 8
+                                        oh_value = 8
+                                except:
+                                    oh = 0
 
-                            vals = {
-                                'employee_id': member.employee_id.id,
-                                'date': turn_date,
-                                'leader_id': team.leader_id.id,
-                                'responsible_id': team.responsible_id.id,
-                                'team_id': team.id,
-                                'name': actual_name,
-                                'schedule1_in_id': line_temp_id.schedule1_in_id.id,
-                                'schedule1_out_id': line_temp_id.schedule1_out_id.id,
-                                'schedule2_in_id': line_temp_id.schedule2_in_id.id,
-                                'schedule2_out_id': line_temp_id.schedule2_out_id.id,
-                                'turn_type_a': line_temp_id.turn_type_a.id,
-                                'turn_type_b': line_temp_id.turn_type_b.id,
-                                'ordinary_hours': oh,
-                                'aditional_hours': aditional_time,
-                                'oh': oh_value,
-                                'fortnight_line_id': fortnight_id.id
-                            }
-                            turn_id = self.create(vals)
-                            created_turn.append(turn_id.id)
-                            count_days += 1
-                if len(created_turn) > 0:
-                    if team.send_email:
-                        self.generate_and_send_report(team, actual_name)
-                    self.send_mail(team, created_turn)
+                                vals = {
+                                    'employee_id': member.employee_id.id,
+                                    'date': turn_date,
+                                    'leader_id': team.leader_id.id,
+                                    'responsible_id': team.responsible_id.id,
+                                    'team_id': team.id,
+                                    'name': actual_name,
+                                    'schedule1_in_id': line_temp_id.schedule1_in_id.id,
+                                    'schedule1_out_id': line_temp_id.schedule1_out_id.id,
+                                    'schedule2_in_id': line_temp_id.schedule2_in_id.id,
+                                    'schedule2_out_id': line_temp_id.schedule2_out_id.id,
+                                    'turn_type_a': line_temp_id.turn_type_a.id,
+                                    'turn_type_b': line_temp_id.turn_type_b.id,
+                                    'ordinary_hours': oh,
+                                    'aditional_hours': aditional_time,
+                                    'oh': oh_value,
+                                    'fortnight_line_id': fortnight_id.id
+                                }
+                                turn_id = self.create(vals)
+                                created_turn.append(turn_id.id)
+                                count_days += 1
+                    if len(created_turn) > 0:
+                        if team.send_email:
+                            self.generate_and_send_report(team, actual_name)
+                        self.send_mail(team, created_turn)
+
+    def validate_name(self, team, turn):
+        tuns_rec_obj =  self.env['hr.turn.registration']
+        rec_ids = tuns_rec_obj.search(['|',('leader_id','=',team.leader_id.id),('responsible_id','=',team.responsible_id.id),('team_id','=',team.id)])
+        if turn in set(rec_ids.mapped('name')):
+            self.generate_and_send_report(team, turn)
+            return True
+        else:
+            return False
 
     @api.model
     def generate_and_send_report(self, team, turn):
         user_id = team.leader_id.user_id.id
         team_id = team
         turn = turn
-        email_to = 'oavilez@cmairlines.com'
 
         notify_id = self.env['turn.email.notifications'].search([])
         emails = notify_id.line_ids.mapped('email')
