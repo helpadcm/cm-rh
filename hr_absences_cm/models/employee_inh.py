c3e1e0d5e458d38d6171287b31678ea7f1de5674
Oniel <helmutpadilla@cmairlines.com>
2025-02-10 17:12:23 -0600

    [FIX]solucion a horas en letras

--- hr_absences_cm/models/employee_inh.py
+++ hr_absences_cm/models/employee_inh.py
@@ -4,7 +4,8 @@ class HrEmployeeInh(models.Model):
     _inherit = 'hr.employee'
 
     compensatory_hours = fields.Float(string="Horas compensatorios Disp.")
-    compensatory_day = fields.Float(string="Equivalente Dias", compute="calculate_days")
+    compensatory_day = fields.Float(string="Eq. Dias", compute="calculate_days")
+    compensatory_day_string = fields.Char(string="Equivalente Dias", compute="calculate_days")
     vacations_day = fields.Integer(string="Vacaciones Disp.")
     program_to_fly = fields.Integer(string="Programa a Volar Disp.")
 
@@ -12,15 +13,20 @@ class HrEmployeeInh(models.Model):
     def calculate_days(self):
         for rec in self:
             if rec.compensatory_hours > 0:
-                rec.compensatory_day =  rec.compensatory_hours / 24
+                rec.compensatory_day =  rec.compensatory_hours / 8
+                days = int(rec.compensatory_hours // 8)
+                hours = rec.compensatory_hours % 8
+                rec.compensatory_day_string = f"{days} dia(s) y {hours} hora(s)"
             else:
                 rec.compensatory_day = 0
+                rec.compensatory_day_string = ''
 
 class employeePublicHRInh(models.Model):
     _inherit = 'hr.employee.public'
 
     compensatory_hours = fields.Float(string="Dias compensatorios Disp.")
-    compensatory_day = fields.Float(string="Equivalente Dias", compute="calculate_days")
+    compensatory_day = fields.Float(string="Eq. Dias", compute="calculate_days")
+    compensatory_day_string = fields.Char(string="Equivalente Dias", compute="calculate_days")
     vacations_day = fields.Integer(string="Vacaciones Disp.")
     program_to_fly = fields.Integer(string="Programa a Volar Disp.")
 
@@ -28,6 +34,10 @@ class employeePublicHRInh(models.Model):
     def calculate_days(self):
         for rec in self:
             if rec.compensatory_hours > 0:
-                rec.compensatory_day =  rec.compensatory_hours / 24
+                rec.compensatory_day =  rec.compensatory_hours / 8
+                days = int(rec.compensatory_hours // 8)
+                hours = rec.compensatory_hours % 8
+                rec.compensatory_day_string = f"{days} dias y {hours} horas"
             else:
                 rec.compensatory_day = 0
+                rec.compensatory_day_string = ''
