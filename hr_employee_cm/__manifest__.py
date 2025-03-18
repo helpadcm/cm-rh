7472f34dd1fd4458bdbb06d2367acd3f27342544
Oniel <helmutpadilla@cmairlines.com>
2025-03-18 15:21:48 -0600

    [IMP]cambio de version

--- hr_employee_cm/__manifest__.py
+++ hr_employee_cm/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Employee module for CM Airlines',
-    'version': '1.0.2',
+    'version': '1.0.1',
     'category': 'Human Resources/Employee',
     'sequence': 245,
     'summary': 'Employee module for CM Airlines',
