ae64a329fddc9fc58eced48edc816c811baefe62
Oniel <helmutpadilla@cmairlines.com>
2025-06-30 11:37:17 -0600

    [IMP]cambio de ip de webservice

--- hr_attendance_cm/wizard/get_marking.py
+++ hr_attendance_cm/wizard/get_marking.py
@@ -33,7 +33,7 @@ class getMarkings(models.TransientModel):
         code_employees = []
         for clock in clocks_ids:
             # url = "http://10.1.4.56:8080/markings?ip_str=%s&port_str=%s&date=%s"%(str(clock.ip_address), str(clock.port), self.date)
-            url = "http://181.189.230.70:8080/markings?ip_str=%s&port_str=%s&date=%s"%(str(clock.ip_address), str(clock.port), self.date)
+            url = "http://181.115.21.90:8080/markings?ip_str=%s&port_str=%s&date=%s"%(str(clock.ip_address), str(clock.port), self.date)
 
             response = requests.get(url)
             if response.status_code == 200:
