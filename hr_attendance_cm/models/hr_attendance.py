c07ac49b667c75d4c1bd256a1d5f08ba44ef4f34
Oniel <helmutpadilla@cmairlines.com>
2025-06-30 11:58:40 -0600

    [IMP]cambio de ip de webservice de marcas

--- hr_attendance_cm/models/hr_attendance.py
+++ hr_attendance_cm/models/hr_attendance.py
@@ -54,7 +54,7 @@ class HrAttendance(models.Model):
         code_employees = []
         for clock in clocks_ids:
             # url = "http://10.1.4.56:8080/markings?ip_str=%s&port_str=%s&date=%s"%(str(clock.ip_address), str(clock.port), last_date)
-            url = "http://181.189.230.70:8080/markings?ip_str=%s&port_str=%s&date=%s"%(str(clock.ip_address), str(clock.port), last_date)
+            url = "http://181.115.21.90:8080/markings?ip_str=%s&port_str=%s&date=%s"%(str(clock.ip_address), str(clock.port), last_date)
 
             response = requests.get(url)
             if response.status_code == 200:
