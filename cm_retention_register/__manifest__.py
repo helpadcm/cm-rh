231bee2644844dab8abbea8fb23d245f78871548
Oniel <helmutpadilla@cmairlines.com>
2025-08-14 15:16:30 -0600

    [ADD]reporte de retenciones

--- cm_retention_register/__manifest__.py
+++ cm_retention_register/__manifest__.py
@@ -7,13 +7,16 @@
     "description": "Gestiona múltiples retenciones aplicadas a pagos desde el wizard de registro.",
     "data": [
         "data/ret_sequence.xml",
+        "data/paperformat.xml",
         "security/ir.model.access.csv",
         "wizard/payment_register_inh_view.xml",
         "views/company_view_inherit.xml",
         "views/payment_view_inh.xml",
         "views/account_account_view.xml",
         "views/retentions_view.xml",
-        "views/menus.xml"
+        "views/menus.xml",
+        "report/report_retention.xml",
+        "report/report_report.xml"
     ],
     "installable": True,
     "application": False,
