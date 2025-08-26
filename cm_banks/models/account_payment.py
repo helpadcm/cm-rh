7d1c55584e1afb075abf329cd12d0b09ed882d72
Oniel <helmutpadilla@cmairlines.com>
2025-08-26 08:28:19 -0600

    [FIX]error al realizar pago multiple de facturas de proveedor

--- cm_banks/models/account_payment.py
+++ cm_banks/models/account_payment.py
@@ -54,10 +54,11 @@ class ap_account_payment(models.Model):
 
 	def action_post(self):
 		res = super(ap_account_payment, self).action_post()
-		if self.partner_type == 'supplier':
-			sequence_id = self.journal_id.sequence_ids.filtered(lambda seq: seq.code2.code == self.pay_method_type)
-			if sequence_id:
-				self.name = sequence_id.next_by_id()
+		for rec in self:
+			if rec.partner_type == 'supplier':
+				sequence_id = rec.journal_id.sequence_ids.filtered(lambda seq: seq.code2.code == rec.pay_method_type)
+				if sequence_id:
+					rec.name = sequence_id.next_by_id()
 		return res
 
 	def _get_shared_move_line_vals(self, debit, credit, amount_currency, move_id, invoice_id=False):
