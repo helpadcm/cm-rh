b2e243c13d574fde63323fefee7af3c7cce4723f
Oniel <helmutpadilla@cmairlines.com>
2025-08-29 15:53:47 -0600

    [IMP]cambios en orden

--- cm_cargo_handling/models/__init__.py
+++ cm_cargo_handling/models/__init__.py
@@ -6,11 +6,11 @@ from . import station
 from . import airport
 from . import rent_product
 from . import product_product
+from . import res_company_inherit
 from . import pricelist_handling
 from . import cm_prepago
 from . import sale_order_handling
 from . import move_inh
-from . import res_company_inherit
 from . import cargo_manifest
 # import configuration_status_inherit
 # import view_data
