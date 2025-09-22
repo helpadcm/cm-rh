572ba8a0eaf210e64c373d046bcdcffece91450c
Oniel <helmutpadilla@cmairlines.com>
2025-09-22 08:33:41 -0600

    [IMP]comentando codigo

--- cm_banks/wizard/register_payment.py
+++ cm_banks/wizard/register_payment.py
@@ -31,37 +31,37 @@ class account_payment_inherit_wizard(models.TransientModel):
         context = dict(self._context or {})
         active_model = context.get('active_model')
         active_ids = context.get('active_ids')
-        invoices = self.env[active_model].browse(active_ids)
+        # invoices = self.env[active_model].browse(active_ids)
         #domain=[('move_id','in',invoices.ids),('full_reconcile_id','=',False),('partner_id','=',rec.get('partner_id'))]
-        if invoices[0].move_type == 'in_invoice':
-            type_account = 'liability_payable'
-        if invoices[0].move_type == 'out_invoice':
-            type_account = 'asset_receivable'
-        move_line_obj = invoices.filtered(lambda line: line.reconciled == False and line.account_id.account_type == type_account)
-        pay_line_ids = []
-        for ml in move_line_obj:
-            vals = {
-                'move_line_id': ml.id,
-                'account_id': ml.account_id.id,
-                'amount_original': ml.move_id.amount_total,
-                'currency_id': ml.currency_id.id,
-                'date_original': ml.date,
-                'date_due': ml.date_maturity,
-                'amount_unreconcilied': ml.move_id.amount_residual,
-                'amount': ml.move_id.amount_residual,
-                'reconcile': True
-            }
-            pay_line_ids.append((0, 0, vals))
+        # if invoices[0].move_type == 'in_invoice':
+        #     type_account = 'liability_payable'
+        # if invoices[0].move_type == 'out_invoice':
+        #     type_account = 'asset_receivable'
+        # move_line_obj = invoices.filtered(lambda line: line.reconciled == False and line.account_id.account_type == type_account)
+        # pay_line_ids = []
+        # for ml in move_line_obj:
+        #     vals = {
+        #         'move_line_id': ml.id,
+        #         'account_id': ml.account_id.id,
+        #         'amount_original': ml.move_id.amount_total,
+        #         'currency_id': ml.currency_id.id,
+        #         'date_original': ml.date,
+        #         'date_due': ml.date_maturity,
+        #         'amount_unreconcilied': ml.move_id.amount_residual,
+        #         'amount': ml.move_id.amount_residual,
+        #         'reconcile': True
+        #     }
+        #     pay_line_ids.append((0, 0, vals))
         rec.update({
             'invoice_compute': [(6, 0, active_ids)],
-            'payment_line_ids': pay_line_ids
+            # 'payment_line_ids': pay_line_ids
         })
         return rec
 
     @api.onchange('journal_id', 'pay_method_type')
     def onchange_journal(self):
         if self.journal_id:
-            self.recalc()
+            # self.recalc()
             if self.partner_type == 'customer':
                 if self.journal_id.sequence_id:
                     sequence_id = self.journal_id.sequence_id
@@ -94,42 +94,42 @@ class account_payment_inherit_wizard(models.TransientModel):
                 else:
                     self.next_number = 'Configure secuencias para el tipo de transaccion que desea realizar'
 
-    def recalc(self):  
-        if self.journal_id.currency_id:
-            currency_id = self.journal_id.currency_id.with_context(date=self.payment_date)
-        else:
-            currency_id = self.journal_id.company_id.currency_id.with_context(date=self.payment_date)
+    # def recalc(self):  
+    #     if self.journal_id.currency_id:
+    #         currency_id = self.journal_id.currency_id.with_context(date=self.payment_date)
+    #     else:
+    #         currency_id = self.journal_id.company_id.currency_id.with_context(date=self.payment_date)
         
-        for pl in self.payment_line_ids:
-            line_currency = pl.move_line_id.currency_id or self.journal_id.company_id.currency_id
-            line_currency_id = pl.currency_id or self.journal_id.company_id.currency_id
+    #     for pl in self.payment_line_ids:
+    #         line_currency = pl.move_line_id.currency_id or self.journal_id.company_id.currency_id
+    #         line_currency_id = pl.currency_id or self.journal_id.company_id.currency_id
 
-            amount_original = pl.move_line_id.move_id.amount_total
-            amount_unreconciled = pl.move_line_id.move_id.amount_residual
+    #         amount_original = pl.move_line_id.move_id.amount_total
+    #         amount_unreconciled = pl.move_line_id.move_id.amount_residual
 
-            pl.amount_original = line_currency._convert(amount_original, currency_id, self.journal_id.company_id, self.payment_date)
-            pl.amount_unreconcilied = line_currency._convert(amount_unreconciled, currency_id, self.journal_id.company_id, self.payment_date)
-            if not pl.reconcile:
-                pl.amount = line_currency_id._convert(pl.amount, currency_id, self.journal_id.company_id, self.payment_date)
-            else:
-                pl.amount = pl.amount_unreconcilied
-            pl.currency_id = currency_id
+    #         pl.amount_original = line_currency._convert(amount_original, currency_id, self.journal_id.company_id, self.payment_date)
+    #         pl.amount_unreconcilied = line_currency._convert(amount_unreconciled, currency_id, self.journal_id.company_id, self.payment_date)
+    #         if not pl.reconcile:
+    #             pl.amount = line_currency_id._convert(pl.amount, currency_id, self.journal_id.company_id, self.payment_date)
+    #         else:
+    #             pl.amount = pl.amount_unreconcilied
+    #         pl.currency_id = currency_id
 
-    @api.depends('can_edit_wizard', 'amount', 'payment_line_ids')
-    def _compute_payment_difference(self):
-        for wizard in self:
-            if wizard.can_edit_wizard and wizard.payment_date:
-                batch_result = wizard._get_batches()[0]
-                lines_amount = 0.0
-                total_amount_residual_in_wizard_currency = wizard\
-                    ._get_total_amount_in_wizard_currency_to_full_reconcile(batch_result, early_payment_discount=False)[0]
+    # @api.depends('can_edit_wizard', 'amount', 'payment_line_ids')
+    # def _compute_payment_difference(self):
+    #     for wizard in self:
+    #         if wizard.can_edit_wizard and wizard.payment_date:
+    #             batch_result = wizard._get_batches()[0]
+    #             lines_amount = 0.0
+    #             total_amount_residual_in_wizard_currency = wizard\
+    #                 ._get_total_amount_in_wizard_currency_to_full_reconcile(batch_result, early_payment_discount=False)[0]
 
-                if wizard.payment_line_ids:
-                    lines_amount = sum(wizard.payment_line_ids.mapped('amount'))
+    #             if wizard.payment_line_ids:
+    #                 lines_amount = sum(wizard.payment_line_ids.mapped('amount'))
                     
-                wizard.payment_difference = total_amount_residual_in_wizard_currency - lines_amount
-            else:
-                wizard.payment_difference = 0.0
+    #             wizard.payment_difference = total_amount_residual_in_wizard_currency - lines_amount
+    #         else:
+    #             wizard.payment_difference = 0.0
 
     @api.depends('write_off_lines', 'payment_difference')
     def _compute_writeoff_amount(self):
@@ -225,41 +225,41 @@ class account_payment_inherit_wizard(models.TransientModel):
                         })
         return payment_vals
 
-    def action_create_payments(self):
-        payments = super().action_create_payments()
+    # def action_create_payments(self):
+    #     payments = super().action_create_payments()
 
-        for wizard in self:
-            if wizard.payment_line_ids:
-                payment = payments[:1]  # en agrupado debe haber solo 1 pago
-                for line in wizard.payment_line_ids:
-                    move_line = line.move_line_id
-                    if not move_line:
-                        continue
+    #     for wizard in self:
+    #         if wizard.payment_line_ids:
+    #             payment = payments[:1]  # en agrupado debe haber solo 1 pago
+    #             for line in wizard.payment_line_ids:
+    #                 move_line = line.move_line_id
+    #                 if not move_line:
+    #                     continue
 
-                    # Línea de factura abierta (cuenta por cobrar/pagar)
-                    inv_lines = move_line.move_id.line_ids.filtered(
-                        lambda l: l.account_internal_type in ('receivable', 'payable') and not l.reconciled
-                    )
+    #                 # Línea de factura abierta (cuenta por cobrar/pagar)
+    #                 inv_lines = move_line.move_id.line_ids.filtered(
+    #                     lambda l: l.account_internal_type in ('receivable', 'payable') and not l.reconciled
+    #                 )
 
-                    # Línea de pago en la misma cuenta
-                    pay_lines = payment.line_ids.filtered(
-                        lambda l: l.account_id == inv_lines.account_id and not l.reconciled
-                    )
+    #                 # Línea de pago en la misma cuenta
+    #                 pay_lines = payment.line_ids.filtered(
+    #                     lambda l: l.account_id == inv_lines.account_id and not l.reconciled
+    #                 )
 
-                    # Reconciliar por el monto definido
-                    (inv_lines + pay_lines).with_context(
-                        manual_amount=line.amount
-                    ).reconcile()
+    #                 # Reconciliar por el monto definido
+    #                 (inv_lines + pay_lines).with_context(
+    #                     manual_amount=line.amount
+    #                 ).reconcile()
 
-        return payments
+    #     return payments
 
-    @api.depends('can_edit_wizard', 'source_amount', 'source_amount_currency', 'source_currency_id', 'company_id', 'currency_id', 'payment_date', 'payment_line_ids')
-    def _compute_amount(self):
-        for wizard in self:
-            if wizard.payment_line_ids:
-                wizard.amount = sum(wizard.payment_line_ids.mapped("amount"))
-            else:
-                super(account_payment_inherit_wizard, wizard)._compute_amount()
+    # @api.depends('can_edit_wizard', 'source_amount', 'source_amount_currency', 'source_currency_id', 'company_id', 'currency_id', 'payment_date', 'payment_line_ids')
+    # def _compute_amount(self):
+    #     for wizard in self:
+    #         if wizard.payment_line_ids:
+    #             wizard.amount = sum(wizard.payment_line_ids.mapped("amount"))
+    #         else:
+    #             super(account_payment_inherit_wizard, wizard)._compute_amount()
 
 class write_off_line(models.TransientModel):
     _name = "account.payment.writeoffline.wizard"
