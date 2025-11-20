4903600df619111fef8c83c590daf42dc10a27f7
Oniel <oavilez@cmairlines.com>
2025-11-20 11:35:47 -0600

    [FIX]correccion en secuencias

--- cm_cai/models/move_inh.py
+++ cm_cai/models/move_inh.py
@@ -134,6 +134,8 @@ class moveInh(models.Model):
                     if inv.internal_number != 'Borrador':
                         inv.write({'name': inv.internal_number})
                     else:
+                        if inv.name in ['Borrador','/']:
+                            inv.name = inv.journal_id.sequence_id.with_context(ir_sequence_date=inv.date).next_by_id()
                         inv.write({'internal_number': inv.name})
             
             if inv.move_type in ['in_invoice']:
