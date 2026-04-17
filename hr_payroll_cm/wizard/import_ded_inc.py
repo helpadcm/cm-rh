083448d9a36f11e2fc40e6f869c63401b75a7221
Oniel <oavilez@cmairlines.com>
2026-04-17 15:16:14 -0600

    [ADD]opcion para agregar monto fijo en formato

--- hr_payroll_cm/wizard/import_ded_inc.py
+++ hr_payroll_cm/wizard/import_ded_inc.py
@@ -28,32 +28,36 @@ class importIncomesDeductions(models.TransientModel):
         ws = wb.active  # toma la primera hoja
 
         # Iterar por filas
+        line_number = 2
         for row in ws.iter_rows(min_row=2, values_only=True):  # min_row=2 para saltar encabezados
-            employee_name, description, ded_type, initial_date, final_date, monthly_amount, amount_total, fees = row
+            print ("///////////////////////////////")
+            print (row)
+            employee_name, description, ded_type, initial_date, final_date, monthly_amount, amount_total, fees, fixed_fee = row
 
             employee_id = False
             type_id = False
             fees_values = False
             fees_qty = 0
+            fixed_fee_value = 0
 
             if not employee_name:
-                raise ValidationError("Debe agregar nombre del empleado")
+                raise ValidationError(f"""Debe agregar nombre del empleado en la linea {line_number}""")
             else:
                 employee_id = self.env['hr.employee'].search([('name','=',employee_name.upper())])
                 if not employee_id:
-                    raise ValidationError(f"""No existe empleado con el nombre {employee_name}""")
+                    raise ValidationError(f"""No existe empleado con el nombre {employee_name} en la linea {line_number}""")
 
             if not ded_type:
-                raise ValidationError("Debe agregar el tipo de deduccion o ingreso")
+                raise ValidationError(f"""Debe agregar el tipo de deduccion o ingreso en la linea {line_number}""")
             else:
                 if self.options == 'deductions':
                     type_id = self.env['hr.salary.attachment.type'].search([('name','=',ded_type)])
                     if not type_id:
-                        raise ValidationError(f"""No existe tipo de deduccion con el nombre {ded_type}""")
+                        raise ValidationError(f"""No existe tipo de deduccion con el nombre {ded_type} en la linea {line_number}""")
                 else:
                     type_id = self.env['hr.payslip.input.type'].search([('name','=',ded_type)])
                     if not type_id:
-                        raise ValidationError(f"""No existe tipo de ingreso con el nombre {ded_type}""")
+                        raise ValidationError(f"""No existe tipo de ingreso con el nombre {ded_type} en la linea {line_number}""")
 
             if not initial_date:
                 raise ValidationError("Debe agregar la fecha de inicio")
@@ -63,14 +67,19 @@ class importIncomesDeductions(models.TransientModel):
 
             if self.options == 'deduction':
                 if not monthly_amount or monthly_amount <= 0:
-                    raise ValidationError("Debe ingresar el monto mensual o debe ser mayor que 0")
+                    raise ValidationError(f"""Debe ingresar el monto mensual o debe ser mayor que 0 en la linea {line_number}""")
 
             if not amount_total or amount_total <= 0:
-                raise ValidationError("Debe ingresar el monto total o debe ser mayor que 0")
+                raise ValidationError(f"""Debe ingresar el monto total o debe ser mayor que 0 en la linea {line_number}""")
 
             if fees:
-                fees_values = True
-                fees_qty = fees
+                if not fixed_fee:
+                    fees_values = True
+                    fees_qty = fees
+                else:
+                    fees_values = True
+                    fees_qty = fees
+                    fixed_fee_value = fixed_fee
 
             if self.options == 'deductions':
                 values = {
@@ -81,7 +90,8 @@ class importIncomesDeductions(models.TransientModel):
                     'monthly_amount': monthly_amount,
                     'total_amount': amount_total,
                     'by_quotes': fees_values,
-                    'quotes_number': fees_qty
+                    'quotes_number': fees_qty,
+                    'fixed_fee': fixed_fee_value
                 }
 
                 deduction_id = self.env['hr.salary.attachment'].create(values)
@@ -108,3 +118,4 @@ class importIncomesDeductions(models.TransientModel):
 
                 if fees_values:
                     income_id.create_plan()
+            line_number += 1
