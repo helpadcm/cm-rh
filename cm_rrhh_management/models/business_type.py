5c9fae00e06fe4ea5e7738d9025aca296fb2c886
Oniel <helmutpadilla@cmairlines.com>
2026-05-05 13:25:28 -0600

    [ADD]modulos

--- cm_rrhh_management/models/business_type.py
+++ cm_rrhh_management/models/business_type.py
@@ -8,9 +8,11 @@ class businessList(models.Model):
 
     name = fields.Char(string="Nombre")
     email = fields.Char(string="Correo de contacto")
-    business_type = fields.Selection([('hotel','Hotel'),('ferry','Ferry')],string="Tipo de negocio")
+    business_type = fields.Selection([('hotel','Hotel'),('ferry','Ferry'),('fly','Vuelos')],string="Tipo de negocio")
     phone = fields.Char(string="Telefono")
     cc_email = fields.Char(string="CC Correos")
+    color = fields.Integer(string='Color', help="The color selected here will be used in every screen with the time off type.")
+    code = fields.Char(string="Codigo")
 
 class ferryRoutes(models.Model):    
     _name = 'cm.ferry.routes'
