74a768128c100c87a87aeec148e940a29158c554
Oniel <helmutpadilla@cmairlines.com>
2026-05-29 09:01:00 -0600

    [IMP]codigo comentado

--- cm_banks/models/account_account.py
+++ cm_banks/models/account_account.py
@@ -3,7 +3,7 @@ from odoo import models, fields, api, exceptions, _
 
 class account_account(models.Model):
 	_inherit = 'account.account'
-	_order= "code asc"
+	# _order= "code asc"
 
 	use_4_retention=fields.Boolean('Usar para retenciones', help="If you check this option, this account will be used for retentions in Miscellaneous checks")
 	tax_description=fields.Char(string='Descripción del impuesto')
