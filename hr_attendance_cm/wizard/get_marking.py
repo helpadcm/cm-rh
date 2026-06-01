8a0f30ba9567a163c75cef54ff26ab0c074b8907
Oniel <helmutpadilla@cmairlines.com>
2026-06-01 01:01:28 -0600

    [IMP]removiendo codigo

--- hr_attendance_cm/wizard/get_marking.py
+++ hr_attendance_cm/wizard/get_marking.py
@@ -6,7 +6,7 @@ import logging
 from odoo.exceptions import UserError, ValidationError
 import pytz
 import requests
-from zk import ZK, const
+# from zk import ZK, const
 _logger = logging.getLogger(__name__)
 # try:
 # except ImportError:
@@ -70,134 +70,6 @@ class getMarkings(models.TransientModel):
 
         if len(markings_values) > 0:
             self.create_real_marking(markings_values)
-
-    # def connect_sql_server(self):
-    #     server = '10.1.4.56'
-    #     database = 'attendance'
-    #     username = 'sa'
-    #     password = "youStrong(@)Password"
-    #     _logger = logging.getLogger(__name__)
-    #     _logger.info("####################Intentando conexion######################")
-    #     try:
-    #         # Crear la conexión
-    #         conn = pymssql.connect(
-    #             server=server,
-    #             user=username,
-    #             password=password,
-    #             database=database,
-    #             port=1433
-    #         )
-    #         _logger.info("Conexión exitosa a SQL Server.")
-    #         for cday in reversed(range(4)):
-    #             check_date = self.date - timedelta(days=cday)
-    #             year = check_date.year
-    #             month = check_date.month
-    #             day = check_date.day
-
-    #             if not self.employee_id:
-    #                 query_sql = """SELECT usertable.NAME as employee,
-    #                             checking.CHECKTIME as date,
-    #                             DATENAME(WEEKDAY,checking.CHECKTIME) as day,
-    #                             machine.MachineAlias as clock,
-    #                             machine.MachineNumber as CODCLOCK,
-    #                             usertable.SSN as codemployee
-    #                         FROM CHECKINOUT as checking
-    #                         INNER JOIN USERINFO as usertable ON checking.USERID = usertable.USERID
-    #                         INNER JOIN Machines as machine ON checking.SENSORID = machine.MachineNumber
-    #                         WHERE checking.CHECKTIME BETWEEN '%s-%s-%s 00:00:00' AND '%s-%s-%s 23:59:59'
-    #                     """%(year,month,day,year,month,day)
-    #             else:
-    #                 query_sql = """SELECT usertable.NAME as employee,
-    #                             checking.CHECKTIME as date,
-    #                             DATENAME(WEEKDAY,checking.CHECKTIME) as day,
-    #                             machine.MachineAlias as clock,
-    #                             machine.MachineNumber as CODCLOCK,
-    #                             usertable.SSN as codemployee
-    #                         FROM CHECKINOUT as checking
-    #                         INNER JOIN USERINFO as usertable ON checking.USERID = usertable.USERID
-    #                         INNER JOIN Machines as machine ON checking.SENSORID = machine.MachineNumber
-    #                         WHERE checking.CHECKTIME BETWEEN '%s-%s-%s 00:00:00' AND '%s-%s-%s 23:59:59' AND usertable.SSN = '%s'
-    #                     """%(year,month,day,year,month,day, self.employee_id.barcode)
-                
-    #             # Ejecutar una consulta de ejemplo
-    #             cursor = conn.cursor()
-    #             cursor.execute(query_sql)
-    #             results = cursor.fetchall()
-    #             code_employees = []
-    #             markings = []
-                
-    #             for row in results:
-    #                 code_emp = row[5]
-    #                 date = row[1]
-    #                 clock = row[4]
-    #                 clock_id = self.env['hr.attendance.device'].search([('device_id','=',clock)])
-    #                 if clock_id:
-    #                     vals = {
-    #                         'date': date,
-    #                         'code_clock': clock
-    #                     }
-    #                     if code_emp in code_employees:
-    #                         markings[code_employees.index(code_emp)]['lines'].append(vals)
-    #                     else:
-    #                         code_employees.append(code_emp)
-    #                         markings.append({
-    #                             'code_employee': code_emp,
-    #                             'date': date,
-    #                             'lines': [vals]
-    #                         })
-
-    #             # Cerrar conexión
-    #             cursor.close()
-    #             if markings:
-    #                 self.create_real_marking(markings, self.option_form)
-    #         conn.close()
-    #     except Exception as e:
-    #         _logger.info(f"Error al conectar a SQL Server: {e}")
-
-    def action_set_timezone(self, machine):
-        """Function to set user's timezone to device"""
-        machine_ip = machine.ip_address
-        zk_port = machine.port
-        try:
-            # Connecting with the device with the ip and port provided
-            zk = ZK(machine_ip, port=zk_port, timeout=15,
-                    password=0,
-                    force_udp=False, ommit_ping=False)
-        except NameError:
-            raise UserError(
-                _("Pyzk module not Found. Please install it"
-                    "with 'pip3 install pyzk'."))
-        conn = self.device_connect(zk)
-        if conn:
-            user_tz = self.env.context.get(
-                'tz') or self.env.user.tz or 'UTC'
-            user_timezone_time = pytz.utc.localize(fields.Datetime.now())
-            user_timezone_time = user_timezone_time.astimezone(pytz.timezone(user_tz))
-            conn.set_time(user_timezone_time)
-            return {
-                'type': 'ir.actions.client',
-                'tag': 'display_notification',
-                'params': {
-                    'message': 'Successfully Set the Time',
-                    'type': 'success',
-                    'sticky': False
-                }
-            }
-        else:
-            raise UserError(_(
-                "Please Check the Connection"))
-
-
-    def device_connect(self, zk):
-        """Function for connecting the device with Odoo"""
-        try:
-            conn = zk.connect()
-            return conn
-        except Exception:
-            _logger = logging.getLogger(__name__)
-            _logger.info("#################### Error de conexion ######################")
-            _logger.info(zk)
-            return False
 
     def create_real_marking(self, markings):
         for mark in markings:
