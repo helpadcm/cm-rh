70a515eef00eb3746fab5632957698bdadcbf9a0
Oniel <helmutpadilla@cmairlines.com>
2026-06-08 16:28:31 -0600

    [IMP]en pagos de proveedor mostrar la referencia y no el numero de factura

--- cm_banks/wizard/register_payment.py
+++ cm_banks/wizard/register_payment.py
@@ -18,12 +18,21 @@ class account_payment_inherit_wizard(models.TransientModel):
                 ('otros','Otros')], string='Tipo de transaccion')
 
     @api.model
-    def _get_batch_communication(self, batch_result):
-        if self.payment_type == 'outbound':
-            labels = set(line.move_id.ref or line.move_id.name for line in batch_result['lines'])
+    def _get_communication(self, lines):
+        ''' Helper to compute the communication based on lines.
+        :param lines:           A recordset of the `account.move.line`'s that will be reconciled.
+        :return:                A string representing a communication to be set on payment.
+        '''
+        if len(lines.move_id) == 1:
+            move = lines.move_id
+            label = move.ref or move.payment_reference or move.name
+        elif any(move.is_outbound() for move in lines.move_id):
+            # outgoing payments references should use moves references
+            labels = {move.ref or move.payment_reference or move.name for move in lines.move_id}
+            return ', '.join(sorted(filter(lambda l: l, labels)))
         else:
-            labels = set(line.move_id.payment_reference or line.name or line.move_id.ref or line.move_id.name for line in batch_result['lines'])
-        return ', '.join(sorted(labels))
+            label = self.company_id.get_next_batch_payment_communication()
+        return label
 
     @api.model
     def default_get(self, fields):
