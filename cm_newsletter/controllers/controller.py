d403cfe37d636fc2204346d6cc7714bf9811f7db
Oniel <helmutpadilla@cmairlines.com>
2026-06-10 14:49:56 -0600

    [IMP]cambio en controller

--- cm_newsletter/controllers/controller.py
+++ cm_newsletter/controllers/controller.py
@@ -7,25 +7,19 @@ _logger = logging.getLogger(__name__)
 
 class getDataNewsletter(http.Controller):
 
-    @http.route('/api/newsletter_data', type='jsonrpc',auth='public',methods=['POST'],csrf=False)
+    @http.route('/api/newsletter_data',type='http',auth='public',methods=['POST'],csrf=False)
     def newsletter_data(self, **kwargs):
         data = json.loads(request.httprequest.data)
-        _logger.info("user: %s")
-        _logger.info("########## Data newsletter: %s ###############", data)
-        email = data.get("email")
-        lenguage = data.get("lenguage")
-        date = data.get("date")
-        country = data.get("country")
+        _logger.info("##################### Data newsletter: %s #####################", data)
 
         request.env['cm.newsletter.list'].sudo().create({
-            'name': email or '',
-            'lenguage': lenguage or '',
-            'date': date or '',
-            'country': country or ''
+            'name': data.get('email', ''),
+            'lenguage': data.get('lenguage', ''),
+            'date': data.get('date', ''),
+            'country': data.get('country', '')
         })
 
-        return {
+        return request.make_json_response({
             'success': True,
-            'message': 'Datos recibidos',
-            'data': data
-        }
+            'message': 'Datos recibidos'
+        })
