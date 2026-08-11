86cd4d1233af261ce92e762c2a3785c8507a169c
Oniel <helmutpadilla@cmairlines.com>
2026-08-11 16:03:26 -0600

    [ADD]mas niveles agregados

--- hr_payroll_cm/models/employee.py
+++ hr_payroll_cm/models/employee.py
@@ -7,14 +7,14 @@ selection_type = [
     ('department','Por departamento'),
 ]
 
-levels = [('1', 'Nivel 1'),('2', 'Nivel 2'),('3', 'Nivel 3'),('4', 'Nivel 4'),('5', 'Nivel 5')]
+levels = [('1', 'Nivel 1'),('2', 'Nivel 2'),('3', 'Nivel 3'),('4', 'Nivel 4'),('5', 'Nivel 5'),('6', 'Nivel 6'),('7', 'Nivel 7'),('8', 'Nivel 8'),('9', 'Nivel 9'),('10', 'Nivel 10')]
 
 class Employee(models.Model):
     _inherit = 'hr.employee'
 
     analytic_account_id = fields.Many2one('account.analytic.account',string="Cuenta Analitica")
     resident_number = fields.Char(string="Número de Residencia")
-    level_number = fields.Selection(levels, string="Nivel del empleado", default="5")
+    level_number = fields.Selection(levels, string="Nivel del empleado", default="10")
 
     def get_employee_no(self):
         for employee in self:
@@ -25,7 +25,7 @@ class employeePublic(models.Model):
 
     analytic_account_id = fields.Many2one('account.analytic.account',string="Cuenta Analitica")
     resident_number = fields.Char(string="Número de Residencia")
-    level_number = fields.Selection(levels, string="Nivel del empleado", default="5")
+    level_number = fields.Selection(levels, string="Nivel del empleado", default="10")
 
 class EmployeeMembersInh(models.Model):
     _inherit = 'hr.employees.members'
