5d2af6f7e634c38e43a139c4ce50b0f55dd8b976
Oniel <helmutpadilla@cmairlines.com>
2026-09-23 14:31:36 -0600

    [IMP]cambio en calculo de antiguedad, meses en base a 30 dias

--- hr_contract_cm/models/employee.py
+++ hr_contract_cm/models/employee.py
@@ -1,6 +1,7 @@
 from odoo import fields, models , api
-from odoo.tools.date_utils import relativedelta
-from datetime import timedelta
+from dateutil.relativedelta import relativedelta
+from datetime import date
+import calendar
 
 class employeeContract(models.Model):
     _inherit = 'hr.employee'
@@ -45,20 +46,41 @@ class employeeContract(models.Model):
         compute='_compute_seniority'
         )
 
-    @api.depends('contract_date_start','contract_date_end')
+    @api.depends('contract_date_start', 'contract_date_end')
     def _compute_seniority(self):
         for employee in self:
             seniority = False
+
             if employee.contract_date_start:
-                date_end = fields.Date.today()
+                date_start = employee.contract_date_start
+                date_end = employee.contract_date_end or fields.Date.today()
 
-                if employee.contract_date_end:
-                    date_end = employee.contract_date_end
+                # Cálculo utilizando:
+                # 1 año = 360 días
+                # 1 mes = 30 días
 
-                date_end -= timedelta(days=1)
-                date = relativedelta(date_end, employee.contract_date_start)
-                seniority = f'{date.years} años, {date.months} meses y {date.days} días'
-            
+                total_days = (
+                    (date_end.year - date_start.year) * 360
+                    + (date_end.month - date_start.month) * 30
+                    + (date_end.day - date_start.day)
+                )
+
+                # Evitar resultados negativos
+                if total_days < 0:
+                    total_days = 0
+
+                years = total_days // 360
+                remaining_days = total_days % 360
+
+                months = remaining_days // 30
+                days = remaining_days % 30
+
+                seniority = (
+                    f'{years} años, '
+                    f'{months} meses y '
+                    f'{days} días'
+                )
+
             employee.seniority = seniority
 
     @api.model
